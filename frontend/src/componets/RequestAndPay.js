ca9c5ce664228b85a1d831a03023cb19dd86b49d
Ashish-Kumar-007 <ashishsahoo2015@gmail.com>
2024-03-21 23:31:53 +0530

    Updated contract function process

--- frontend/src/componets/RequestAndPay.js
+++ frontend/src/componets/RequestAndPay.js
@@ -1,49 +1,105 @@
 import React, { useState, useEffect } from "react";
 import { DollarOutlined, SwapOutlined } from "@ant-design/icons";
 import { Modal, Input, InputNumber } from "antd";
-import { usePrepareContractWrite, useContractWrite, useWaitForTransaction  } from "wagmi";
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+  useAccount,
+} from "wagmi";
 import { polygonMumbai } from "@wagmi/chains";
 import ABI from "../abi.json";
+import { Web3 } from 'web3';
+
+//private RPC endpoint 
+// const web3 = new Web3('https://rpc-mumbai.maticvigil.com'); 
 
 function RequestAndPay({ requests, getNameAndBalance }) {
+  const {address} = useAccount();
   const [payModal, setPayModal] = useState(false);
   const [requestModal, setRequestModal] = useState(false);
   const [requestAmount, setRequestAmount] = useState(5);
   const [requestAddress, setRequestAddress] = useState("");
   const [requestMessage, setRequestMessage] = useState("");
+  const[isRequested, setIsRquested]= useState(false);
+  const [isRequestSuccess, setIsRequestSuccess] = useState(false);
 
-  const { config } = usePrepareContractWrite({
-    chainId: polygonMumbai.id,
-    address: "0x61E05990393693287D829363e2271D370b5F8af3",
-    abi: ABI,
-    functionName: "payRequest",
-    args: [0],
-    overrides: {
-      value: String(Number(requests["1"][0] * 1e18)),
-    },
-  });
+  // const setupWeb3 = async () => {
+  //   const Web3 = require("web3");
+  //   if (window.ethereum) {
+  //     await window.ethereum.request({ method: "eth_requestAccounts" });
+  //   }
+  //   return new Web3('https://rpc-mumbai.maticvigil.com');
+  // };
 
-  const { write, data } = useContractWrite(config);
+  const sendRequest = async () => {
+    const web3 = new Web3(window.ethereum); 
+    const contract = new web3.eth.Contract(
+      ABI,
+      "0x61E05990393693287D829363e2271D370b5F8af3"
+    );
+    const requestAmountInWei = web3.utils.toWei(requestAmount.toString(), "ether")
+    await contract.methods
+      .createRequest(requestAddress, requestAmountInWei, requestMessage)
+      .send({
+        from: address
+      })
+      .then(function (receipt) {
+        setIsRquested(true)
+      });
+  };
 
-  const { config: configRequest } = usePrepareContractWrite({
-    chainId: polygonMumbai.id,
-    address: "0x61E05990393693287D829363e2271D370b5F8af3",
-    abi: ABI,
-    functionName: "createRequest",
-    args: [requestAddress, requestAmount, requestMessage],
-  });
+  const payRequest = async () => {
+    const web3 = new Web3(window.ethereum); 
+    const contract = new web3.eth.Contract(
+      ABI,
+      "0x61E05990393693287D829363e2271D370b5F8af3"
+    );
+    let stringValue = requests["1"][0];
+    console.log(stringValue)
 
-  const { write: writeRequest, data: dataRequest } = useContractWrite(configRequest);
+    await contract.methods
+      .payRequest(0)
+      .send({
+        from: address,
+        value: stringValue
+      })
+      .then(function (receipt) {
+        setIsRequestSuccess(true)
+      });
+  };
 
+  // const { config } = usePrepareContractWrite({
+  //   chainId: polygonMumbai.id,
+  //   address: "0x61E05990393693287D829363e2271D370b5F8af3",
+  //   abi: ABI,
+  //   functionName: "payRequest",
+  //   args: [0],
+  //   overrides: {
+  //     value: String(Number(requests["1"][0] * 1e18)),
+  //   },
+  // });
 
-  const { isSuccess } = useWaitForTransaction({
-    hash: data?.hash,
-  })
+  // const { write, data } = useContractWrite(config);
 
-  const { isSuccess: isSuccessRequest } = useWaitForTransaction({
-    hash: dataRequest?.hash,
-  })
+  // const { config: configRequest } = usePrepareContractWrite({
+  //   chainId: polygonMumbai.id,
+  //   address: "0x61E05990393693287D829363e2271D370b5F8af3",
+  //   abi: ABI,
+  //   functionName: "createRequest",
+  //   args: [requestAddress, requestAmount, requestMessage],
+  // });
 
+  // const { write: writeRequest, data: dataRequest } =
+  //   useContractWrite(configRequest);
+
+  // const { isSuccess } = useWaitForTransaction({
+  //   hash: data?.hash,
+  // });
+
+  // const { isSuccess: isSuccessRequest } = useWaitForTransaction({
+  //   hash: dataRequest?.hash,
+  // });
 
   const showPayModal = () => {
     setPayModal(true);
@@ -59,11 +115,11 @@ function RequestAndPay({ requests, getNameAndBalance }) {
     setRequestModal(false);
   };
 
-  useEffect(()=>{
-    if(isSuccess || isSuccessRequest){
+  useEffect(() => {
+    if (isRequestSuccess || isRequested) {
       getNameAndBalance();
     }
-  },[isSuccess, isSuccessRequest])
+  }, [isRequestSuccess, isRequested]);
 
   return (
     <>
@@ -71,7 +127,7 @@ function RequestAndPay({ requests, getNameAndBalance }) {
         title="Confirm Payment"
         open={payModal}
         onOk={() => {
-          write?.();
+          payRequest?.();
           hidePayModal();
         }}
         onCancel={hidePayModal}
@@ -90,7 +146,7 @@ function RequestAndPay({ requests, getNameAndBalance }) {
         title="Request A Payment"
         open={requestModal}
         onOk={() => {
-          writeRequest?.();
+          sendRequest?.();
           hideRequestModal();
         }}
         onCancel={hideRequestModal}
@@ -98,11 +154,22 @@ function RequestAndPay({ requests, getNameAndBalance }) {
         cancelText="Cancel"
       >
         <p>Amount (Matic)</p>
-        <InputNumber value={requestAmount} onChange={(val)=>setRequestAmount(val)}/>
+        <InputNumber
+          value={requestAmount}
+          onChange={(val) => setRequestAmount(val)}
+        />
         <p>From (address)</p>
-        <Input placeholder="0x..." value={requestAddress} onChange={(val)=>setRequestAddress(val.target.value)}/>
+        <Input
+          placeholder="0x..."
+          value={requestAddress}
+          onChange={(val) => setRequestAddress(val.target.value)}
+        />
         <p>Message</p>
-        <Input placeholder="Lunch Bill..." value={requestMessage} onChange={(val)=>setRequestMessage(val.target.value)}/>
+        <Input
+          placeholder="Lunch Bill..."
+          value={requestMessage}
+          onChange={(val) => setRequestMessage(val.target.value)}
+        />
       </Modal>
       <div className="requestAndPay">
         <div
